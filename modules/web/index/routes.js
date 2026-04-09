14a5bb4f744abfa674098328a49468027621a33e
Rfaze07 <renzofazele@gmail.com>
2026-04-09 19:57:01 -0300

    cambios index

--- modules/web/index/routes.js
+++ modules/web/index/routes.js
@@ -7,4 +7,10 @@ const { auth, checkAcceso } = require('../../../middlewares');
 router.get('/', controller.getLista);
 router.get('/web', controller.getLista);
 
+router.get('/web/comision-directiva', controller.getComisionDirectiva);
+router.get('/web/historia', controller.getHistoria);
+router.get('/web/horarios', controller.getHorarios);
+router.get('/web/instalaciones', controller.getInstalaciones);
+
+
 module.exports = router
